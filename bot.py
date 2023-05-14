79490b0dc8ba4248eb654752c95ec6afad70288d
gogoyellowscreen <39678753+gogoyellowscreen@users.noreply.github.com>
2023-05-14 15:30:39 +0300

    Gogoyellowscreen/select date UI (#3)
    * Follow PEP-8 empty lines rule
    
    * Add last events per user dictionary
    
    * Add calendar third-party to show UI
    
    ---------
    
    Co-authored-by: Roman Tikhonov <ggys@yandex-team.ru>

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import os
 import sys
 import argparse
 from datetime import datetime
+from telegram_bot_calendar import DetailedTelegramCalendar, LSTEP
 
 import telebot
 
@@ -27,6 +28,8 @@ def logInfo(message):
 bot = telebot.TeleBot(os.getenv('API_TELEGRAM_TOKEN'))
 
 user_data = {}
+last_events = {}
+
 
 # Command to start the bot
 @bot.message_handler(commands=['start'])
@@ -41,11 +44,13 @@ def start(message):
     except Exception:
         logError(sys.exc_info()[1])
 
+
 # Command to display the available commands
 @bot.message_handler(commands=['help'])
 def help(message):
     # Send a list of available commands to the user
     bot.send_message(message.chat.id, 'Available commands:\n/new_event - add a new event to the calendar\n/view_events - view all events in the calendar')
+
 
 # Command to add a new event to the calendar
 @bot.message_handler(commands=['new_event'])
@@ -60,32 +65,44 @@ def new_event(message):
 
 def get_event_name(message):
     try:
+        last_events[message.chat.id] = {'name': message.text}
         # Ask the user for the date of the event
-        bot.send_message(message.chat.id, 'What is the date of the event? (dd/mm/yyyy)')
-        bot.register_next_step_handler(message, get_event_date, message.text)
+        calendar, step = DetailedTelegramCalendar().build()
+        bot.send_message(message.chat.id,
+                         f"Select {LSTEP[step]}",
+                         reply_markup=calendar)
     except Exception:
         logError(sys.exc_info()[1])
 
-def get_event_date(message, event_text):
+
+@bot.callback_query_handler(func=DetailedTelegramCalendar.func())
+def get_event_date(c):
     try:
-        # Convert the user input to a datetime object
-        event_date = datetime.strptime(message.text, '%d/%m/%Y')
-        # Store the event date in user data
-        new_event = {}
-        new_event['date'] = event_date
-        new_event['name'] = event_text
-        events = user_data[message.chat.id].get('events', [])
-        events.append(new_event)
-        user_data[message.chat.id]['events'] = events
-        # Send a confirmation message to the user
-        bot.send_message(message.chat.id, f'Event "{event_text}" added to the calendar on {event_date.strftime("%d/%m/%Y")}.')
-    except ValueError:
-        # Send an error message to the user if the input is not a valid date
-        bot.send_message(message.chat.id, 'Invalid date format. Please enter the date in dd/mm/yyyy format.')
-        # Ask the user for the date again
-        bot.register_next_step_handler(message, get_event_date)
+        event_date, key, step = DetailedTelegramCalendar().process(c.data)
+        if not event_date and key:
+            bot.edit_message_text(f"Select {LSTEP[step]}",
+                                  c.message.chat.id,
+                                  c.message.message_id,
+                                  reply_markup=key)
+        elif event_date:
+            # Store the event date in user data
+            current_event = last_events.get(c.message.chat.id, {})
+            if not current_event:
+                logError(sys.exc_info()[1])
+                bot.send_message(c.message.chat.id, 'Something went wrong, try to start again.')
+                return
+            current_event['date'] = event_date
+            events = user_data[c.message.chat.id].get('events', [])
+            events.append(dict(current_event))
+            user_data[c.message.chat.id]['events'] = events
+            # Send a confirmation message to the user
+            event_text = current_event['name']
+            bot.edit_message_text(f'Event "{event_text}" added to the calendar on {event_date.strftime("%d/%m/%Y")}.',
+                                  c.message.chat.id,
+                                  c.message.message_id)
     except Exception:
         logError(sys.exc_info()[1])
+
 
 # Command to view all events in the calendar
 @bot.message_handler(commands=['view_events'])
@@ -101,6 +118,7 @@ def view_events(message):
             bot.send_message(message.chat.id, f'Events in the calendar:\n{event_list}')
     except Exception:
         logError(sys.exc_info()[1])
+
 
 # Start the bot
 bot.polling()
