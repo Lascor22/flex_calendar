dc2c388912ed9fc6f0da44466a2eb9b4ee6401dd
Roman Tikhonov <ggys@yandex-team.ru>
2023-05-14 15:04:03 +0300

    Add calendar third-party to show UI

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import os
 import sys
 import argparse
 from datetime import datetime
+from telegram_bot_calendar import DetailedTelegramCalendar, LSTEP
 
 import telebot
 
@@ -66,33 +67,39 @@ def get_event_name(message):
     try:
         last_events[message.chat.id] = {'name': message.text}
         # Ask the user for the date of the event
-        bot.send_message(message.chat.id, 'What is the date of the event? (dd/mm/yyyy)')
-        bot.register_next_step_handler(message, get_event_date)
+        calendar, step = DetailedTelegramCalendar().build()
+        bot.send_message(message.chat.id,
+                         f"Select {LSTEP[step]}",
+                         reply_markup=calendar)
     except Exception:
         logError(sys.exc_info()[1])
 
 
-def get_event_date(message):
+@bot.callback_query_handler(func=DetailedTelegramCalendar.func())
+def get_event_date(c):
     try:
-        # Convert the user input to a datetime object
-        event_date = datetime.strptime(message.text, '%d/%m/%Y')
-        # Store the event date in user data
-        current_event = last_events.get(message.chat.id, {})
-        if not current_event:
-            # TODO: log error.
-            return
-        current_event['date'] = event_date
-        events = user_data[message.chat.id].get('events', [])
-        events.append(dict(current_event))
-        user_data[message.chat.id]['events'] = events
-        # Send a confirmation message to the user
-        event_text = current_event['name']
-        bot.send_message(message.chat.id, f'Event "{event_text}" added to the calendar on {event_date.strftime("%d/%m/%Y")}.')
-    except ValueError:
-        # Send an error message to the user if the input is not a valid date
-        bot.send_message(message.chat.id, 'Invalid date format. Please enter the date in dd/mm/yyyy format.')
-        # Ask the user for the date again
-        bot.register_next_step_handler(message, get_event_date)
+        event_date, key, step = DetailedTelegramCalendar().process(c.data)
+        if not event_date and key:
+            bot.edit_message_text(f"Select {LSTEP[step]}",
+                                  c.message.chat.id,
+                                  c.message.message_id,
+                                  reply_markup=key)
+        elif event_date:
+            # Store the event date in user data
+            current_event = last_events.get(c.message.chat.id, {})
+            if not current_event:
+                logError(sys.exc_info()[1])
+                bot.send_message(c.message.chat.id, 'Something went wrong, try to start again.')
+                return
+            current_event['date'] = event_date
+            events = user_data[c.message.chat.id].get('events', [])
+            events.append(dict(current_event))
+            user_data[c.message.chat.id]['events'] = events
+            # Send a confirmation message to the user
+            event_text = current_event['name']
+            bot.edit_message_text(f'Event "{event_text}" added to the calendar on {event_date.strftime("%d/%m/%Y")}.',
+                                  c.message.chat.id,
+                                  c.message.message_id)
     except Exception:
         logError(sys.exc_info()[1])
 
